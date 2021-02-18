db50c20cf8ad382c803deeb1b2412383fd2a3d38
Christian <cwooddel@u.rochester.edu>
2021-02-17 20:53:23 -0500

    JS hw

---
+++ Week_8/W8D4/introJSpart2.js
@@ -0,0 +1,34 @@
+// titleize 
+function titleize(names, callback) {
+    let titleized = names.map(name => `Mx. ${name} Jingleheimer Schmidt`);
+    callback(titleized);
+};
+
+titleize(["Mary", "Brian", "Leo"], (names) => {
+    names.forEach(name => console.log(name));
+});
+
+// elephant 
+
+function Elephant(name, height, tricks) {
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
+};
+
+Elephant.prototype.trumpet = function() {
+    console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!`)
+};
+
+Elephant.prototype.grow = function() {
+    this.height = this.height + 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+    this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function () {
+    trickIndex = Math.floor(Math.random() * this.tricks.length);
+    console.log(`${this.name} is ${this.tricks[trickIndex]}!`);
+};
