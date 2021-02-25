fd2cc73f16702761b84a79a034182722371a625a
Christian <cwooddel@u.rochester.edu>
2021-02-24 20:45:46 -0500

    finish w9d4 hw

--- Week_9/W9D4/skeleton 2/js/index.js
+++ Week_9/W9D4/skeleton 2/js/index.js
@@ -1,6 +1,14 @@
 console.log("Hello from the JavaScript console!");
 
-// Your AJAX request here
-
-// Add another console log here, outside your AJAX request
-console.log("hey bestie xD")
+$.ajax({
+    type: 'GET',
+    url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b",
+    success(data) {
+        console.log("Here's your weather bestie!")
+        console.log(data);
+    },
+    error() {
+        console.error("Sorry bestie that didn't work");
+    },
+})
+console.log("good night bestie xD")
