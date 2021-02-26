d2b32df73ec58688324018c9e5d9208966d70725
Christian <cwooddel@u.rochester.edu>
2021-02-25 19:46:50 -0500

    hw continue

--- Week_9/W9D5/skeleton 2/script.js
+++ Week_9/W9D5/skeleton 2/script.js
@@ -18,7 +18,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // adding SF places as list items
 
-  // --- your code here!
+
+  const handleFavoriteSubmit = (e) => {
+    e.preventDefault();
+
+    const favoriteInput = document.querySelector(".favorite-input");
+    const favorite = favoriteInput.value;
+    favoriteInput.value = "";
+
+    const newListLi = document.createElement("li");
+    newListLi.textContent = favorite;
+
+    const favoritesList = document.getElementById("sf-places");
+    favoritesList.appendChild(newListLi);
+  };
+
+  const listSubmitButton = document.querySelector(".favorite-submit");
+  listSubmitButton.addEventListener("click", handleFavoriteSubmit);
 
 
 
