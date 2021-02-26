947e056806d3a62b8b4526a306fa9ea24cf36b99
Christian <cwooddel@u.rochester.edu>
2021-02-25 19:47:26 -0500

    finish hw

--- Week_9/W9D5/skeleton 2/script.js
+++ Week_9/W9D5/skeleton 2/script.js
@@ -40,7 +40,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // adding new photos
 
-  // --- your code here!
+  const showPhotoForm = (e) => {
+    const photoFormDiv = document.querySelector(".photo-form-container");
+    if (photoFormDiv.className === "photo-form-container") {
+      photoFormDiv.className = "photo-form-container hidden";
+    } else {
+      photoFormDiv.className = "photo-form-container";
+    }
+  };
+
+  const photoFormShowButton = document.querySelector(".photo-show-button");
+  photoFormShowButton.addEventListener("click", showPhotoForm);
+
+
+  const handlePhotoSubmit = (e) => {
+    e.preventDefault();
+
+    const photoUrlInput = document.querySelector(".photo-url-input");
+    const photoUrl = photoUrlInput.value;
+    photoUrlInput.value = "";
+
+    const newImg = document.createElement("img");
+    newImg.src = photoUrl;
+
+    const newPhotoLi = document.createElement("li");
+    newPhotoLi.appendChild(newImg);
+
+    const dogPhotosList = document.querySelector(".dog-photos");
+    dogPhotosList.appendChild(newPhotoLi);
+  };
+
+  const photoSubmitButton = document.querySelector(".photo-url-submit");
+  photoSubmitButton.addEventListener("click", handlePhotoSubmit);
+});
 
 
 
