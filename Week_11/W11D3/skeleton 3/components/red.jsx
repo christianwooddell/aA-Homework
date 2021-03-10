dc88913e9b60bc0627068d28eef43d940cd40a5d
Christian <cwooddel@u.rochester.edu>
2021-03-09 21:09:38 -0500

    finish hw

--- Week_11/W11D3/skeleton 3/components/red.jsx
+++ Week_11/W11D3/skeleton 3/components/red.jsx
@@ -8,9 +8,11 @@ class Red extends React.Component {
     return(
       <div>
         <h2 className="red"></h2>
-        {/* Links here */}
-
-        {/* Routes here */}
+        <NavLink exact to='/red' >Red only</NavLink>
+        <NavLink to='/red/orange' >Add orange</NavLink>
+        <NavLink to='/red/yellow' >Add yellow</NavLink>
+        <Route path="/red/orange" component={Orange} />
+        <Route path="/red/yellow" component={Yellow} />
       </div>
     );
   }
